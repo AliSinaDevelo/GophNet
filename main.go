a993cf8b46981b86d90d5856dc995a2600cba141
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-07-23 02:45:10 +0200

    utils included in main.go, some typos fixed

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 	"GophNet/server"
+	"GophNet/utils"
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
 	// create the server
 	server.SetupRoutes()
 
+	
 	// run the server
 	addr := fmt.Sprintf(":%d", port)
 	fmt.Printf("Starting GophNet server on http://localhost:%d\n", port)
